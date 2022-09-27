bae4a5594e0b83f49a92bb289a9151030836f0da
Julia Rulent <jrule@noc.ac.uk>
2022-09-27 16:42:44 +0100

    changes to environment.m

--- SRIL34_physics/EXTRA_TOOLS/DOMAIN/environment.m
+++ SRIL34_physics/EXTRA_TOOLS/DOMAIN/environment.m
@@ -1,15 +1,13 @@
-% Note: this is based on the GCOMS1K repository https://github.com/NOC-MSM/GCOMS1k/wiki/GCOMS1k-Domains
-
 addpath(genpath('/login/jdha/matlab/new_matlab/utilities/ann_mwrapper'))
 
 git_path='/work/<user>/GCOMS1k/STARTFILES/';
 projects_path='/work/<user>/GCOMS1k/';
 
 domain_path='/work/<user>/GCOMS1k/INPUTS/';
-domain_outpath='/work/<user>GCOMS1k/OUTPUT/';
+domain_outpath='/work/<user>/GCOMS1k/OUTPUT/';
 assess_path='/work/<user>/GCOMS1k/ASSESSMENT/';
 
-parent_path_coords='<PATH>';
-parent_path='<PATH>';
+parent_path_coords='/projectsa/NEMO/gmaya/ORCA12/2013/';
+parent_path='/work/jholt/JASMIN/NEMO-ORCA0083-N006/';
 Re=6367456.0*pi/180;
 tiny=1e-6;
